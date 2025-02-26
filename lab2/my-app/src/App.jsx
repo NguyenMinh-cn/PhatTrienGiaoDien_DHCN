8d2f207c145b95f958af86c0ede0d0044d51d625
NguyenMinh-cn <nguyengochongminh@gmail.com>
2025-02-26 16:45:55 +0700

    Update App.jsx

--- lab2/my-app/src/App.jsx
+++ lab2/my-app/src/App.jsx
@@ -1,44 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import Button from "./assets/components/Button";
+import "./App.css";
 
 function App() {
   const [num1, setNum1] = useState(0);
   const [num2, setNum2] = useState(0);
   const [result, setResult] = useState(null);
 
-  function handleChange(e){
+  const handleCalculate = (operator) => {
     let kq;
-    if(e==="+"){
-      kq=num1+num2;
+    switch (operator) {
+      case "+":
+        kq = num1 + num2;
+        break;
+      case "-":
+        kq = num1 - num2;
+        break;
+      case "*":
+        kq = num1 * num2;
+        break;
+      case "/":
+        kq = num2 !== 0 ? num1 / num2 : "Lỗi chia cho 0";
+        break;
+      default:
+        kq = "Lỗi";
     }
-    
-
-  }
+    setResult(kq);
+  };
 
   return (
     <>
-    <div className='caculator'>
-    <input id="a" type="text" placeholder='Nhập số thứ nhất'/>
-      <br />
-      <input id="b" type="text" placeholder='Nhập số thứ hai'/>
-      
-    </div>
-    <div>
-    <br />
-      <button onClick={handleClick}>+</button>
-      
-      <button onClick={handleClick}>-</button>
-      <br />
-      <button onClick={handleClick}>*</button>
-      
-      <button onClick={handleClick}>/</button>
-      <br />
-    </div>
+      <div className="calculator">
+        <input
+          type="text"
+          placeholder="Nhập số thứ nhất"
+          value={num1}
+          onChange={(e) => setNum1(Number(e.target.value))}
+        />
+        <br />
+        <input
+          type="text"
+          placeholder="Nhập số thứ hai"
+          value={num2}
+          onChange={(e) => setNum2(Number(e.target.value))}
+        />
+      </div>
 
+      <div>
+        <br />
+        <Button label="+" onClick={handleCalculate} />
+        <Button label="-" onClick={handleCalculate} />
+        <br />
+        <Button label="*" onClick={handleCalculate} />
+        <Button label="/" onClick={handleCalculate} />
+        <br />
+      </div>
+
+      <h2>Kết quả: {result}</h2>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
