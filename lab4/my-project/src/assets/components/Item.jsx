5dc9167d0e48a52495b47857682af4d95fa0959d
Hong Minh <nguyengochongminh@gmail.com>
2025-03-07 20:24:53 +0700

    làm xong phần hiển thị món ăn

--- lab4/my-project/src/assets/components/Item.jsx
+++ lab4/my-project/src/assets/components/Item.jsx
@@ -1,14 +1,18 @@
-import React from "react";
-import "./Item_css.css";
+import React from 'react';
+import './Item_css.css';
 export default function Item({ id, name, image }) {
     return (
         <div className="boder">
-            <img height='100px' width='100%' src={image} />
-            <div className="title-infor" >
+            <img height="100px" width="100%" src={image} />
+            <div className="title-infor">
                 <p>{name}</p>
-                <img height='20px' width='20px' src="https://res.cloudinary.com/dqlzcgear/image/upload/v1741177081/Icon_Button_73_g4khgt.png" alt="" />
+                <img
+                    height="20px"
+                    width="20px"
+                    src="https://res.cloudinary.com/dqlzcgear/image/upload/v1741177081/Icon_Button_73_g4khgt.png"
+                    alt=""
+                />
             </div>
-            
         </div>
     );
-};
+}
--- lab4/my-project/src/assets/components/Menu.jsx
+++ lab4/my-project/src/assets/components/Menu.jsx
@@ -1,30 +1,25 @@
-import React, { useState, useEffect } from "react";
-import Item from "./Item";
-
+import React, { useState, useEffect } from 'react';
+import Item from './Item';
+import './Menu_css.css';
 export default function Menu() {
-    const [arr, setArr] = useState([]); 
-    const url = "https://67c83d350acf98d070858d3b.mockapi.io/a";
+    const [arr, setArr] = useState([]);
+    const url = 'https://67c83d350acf98d070858d3b.mockapi.io/a';
 
     useEffect(() => {
-
         fetch(url)
             .then((response) => response.json())
             .then((data) => {
                 console.log(data);
-                setArr(data);  
+                setArr(data);
             })
-            .catch((error) => console.error("Error fetching data:", error));
-    }, []); 
+            .catch((error) => console.error('Error fetching data:', error));
+    }, []);
 
     return (
-        <div>
-            <ul>
-                {arr.map((e, index) => (
-                    <li key={index}>
-                        <Item id={index} name={e.name} image={e.img} /> 
-                    </li>
-                ))}
-            </ul>
+        <div className="menu-container">
+            {arr.map((e, index) => (
+                <Item key={index} id={index} name={e.name} image={e.img} />
+            ))}
         </div>
     );
 }
